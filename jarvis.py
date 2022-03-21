c0c76bea0dbb4fa45f4c908c946d58e0a3aacf72
nishchayjeet01 <90867938+nishchayjeet01@users.noreply.github.com>
2022-03-21 23:39:37 +0530

    update
    A voice command based virtual assistant.
    Performs various tasks such as web searching, maneuvering through
    directories, sending emails, opening applications etc. For instance the command to open an application
    is:-”open <name-of-application>”.

---
+++ jarvis.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 29 10:35:56 2021
+@author: Nitro 7
+"""
+
+import speech_recognition as sr
+import pyttsx3
+from scripts import * 
+import datetime
+import wikipedia
+import webbrowser
+import pyjokes
+
+r = sr.Recognizer()
+
+
+def speak(data):
+    engine = pyttsx3.init()
+    engine.say(data)
+    engine.runAndWait()
+starting()
+ask_name()
+speak('How can i help you')
+while 1:
+    cmd = get_command() 
+    cmd =  cmd.lower()
+    if cmd=='':
+        speak("sorry I didn't get it please repeat")
+    else:
+        if 'send a mail' in cmd or 'send email' in cmd or 'mail' in cmd or 'email' in cmd:
+            try:
+                speak("what is the message sir?")
+                message = get_command()
+                speak("whome should i send")
+                to = input()
+                send_email(to,message)
+                speak("Email send successfully")
+            except Exception as e:
+                print(e)
+                speak("sorry sir i am unable to send email")
+        elif 'wikipedia' in cmd:
+            speak('Searching Wikipedia...')
+            cmd = cmd.replace('wikipedia',"")
+            result = wikipedia.summary(cmd,sentences=1)
+            speak("According to wikipedia")
+            print(result)
+            speak(result)
+        elif 'youtube' in cmd:
+            speak('opening youtube')
+            webbrowser.open('youtube.com')    
+        elif 'google' in cmd:
+            speak('opening google')
+            webbrowser.open('google.com')   
+        elif 'how are you' in cmd:
+            speak('I am fine sir,Thank you')
+            speak('how are you sir')
+        elif 'fine' in cmd or 'good' in cmd:
+            speak("Its good to know that your fine")    
+        elif 'time' in cmd:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S") 
+            speak(f'Time is {strTime}')
+        elif 'joke' in cmd:
+            speak('Searching for a joke')
+            speak('i get it')
+            speak(pyjokes.get_joke())
+            speak('hahahahahahah')
+        elif 'search' in cmd or 'play' in cmd:
+            cmd = cmd.replace('search','')
+            cmd = cmd.replace('play','')
+            cmd = cmd.replace(' ','')
+            cmd = cmd+".com"
+            webbrowser.open(cmd)
+        elif 'who created you' in cmd or 'who made you' in cmd:
+            speak('I have been created by Nishchay Jeet Singh')
+        elif 'who is palak' in cmd or 'malhar' in cmd or 'bhola' in cmd:
+            speak('she is your best friend since you were kids, sir')
+        elif 'stop' in cmd or 'exit' in cmd:
+            speak('Good Bye sir')
+            break
---
+++ scripts.py
@@ -0,0 +1,61 @@
+import datetime
+import pyttsx3
+import speech_recognition as sr
+import smtplib
+# import credential
+user_name = ""
+
+def speak(data):
+    engine = pyttsx3.init()
+    engine.say(data)
+    engine.runAndWait()
+
+def starting():
+    hr = int(datetime.datetime.now().hour)
+    if hr>=0 and hr<12:
+        speak("Good morning sir!")
+    elif hr>=12 and hr<18:
+        speak("Good Afternoon sir!")
+    else:
+        speak("Good Evening sir!")
+    speak("I am your assistant Jarvis")  
+
+def ask_name():
+    speak("What should i call you sir")
+    while 1:
+        name = get_command()
+        if name!='':
+            arg = 'hello '+name
+            global user_name    
+            user_name = name
+            speak(arg)
+            break
+        else: 
+            speak("Sorry I didn't get it, Please try again")
+
+def get_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        try:
+            print("Speak...")
+            r.adjust_for_ambient_noise(source, duration=0.2)
+            audio = r.listen(source)
+            text = r.recognize_google(audio,language='en-in')
+            text = text.lower()
+            print("Did you say: "+text)
+            return text
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+            return ''
+            
+        except sr.UnknownValueError:
+            print("unknown error occured")    
+            return ''
+
+def send_email(to,message):
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.login(credential.email,credential.password)
+    s.sendmail(credential.email,to,message)
+    s.quit()
